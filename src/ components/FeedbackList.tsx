329b111b4e8511da2b89dea4aa096d7faeb0e5f1
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-07 13:00:14 +0530

    added loading and error state

--- src/ components/FeedbackList.tsx
+++ src/ components/FeedbackList.tsx
@@ -1,27 +1,38 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
+    setLoading(true);
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
         return response.json();
       })
       .then((data) => {
         setLoading(false);
         setFeedbackItems(data.feedbacks);
+      })
+      .catch(() => {
+        setLoading(false);
+        setError("Something went wrong");
       });
   }, []);
 
   return (
     <ol className="feedback-list">
       {loading ? <Spinner /> : null}
+      {error ? <ErrorMessage message={error} /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
