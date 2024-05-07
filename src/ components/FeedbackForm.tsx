a40aea21f8344351f49d51149c4c94744ee23aed
Ashirwad Pradhan <ashirwad.pradhan007@gmail.com>
2024-05-07 11:26:45 +0530

    added feedback self controlled input

--- src/ components/FeedbackForm.tsx
+++ src/ components/FeedbackForm.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 function FeedbackForm() {
+  const [feedback, setFeedback] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setFeedback(e.target.value);
+  };
+
+  const charactersLeft = MAX_CHARACTERS - feedback.length;
   return (
     <form className="form">
       <textarea
+        value={feedback}
         id="feedback-textarea"
         placeholder="h"
         spellCheck={false}
+        onChange={handleChange}
       ></textarea>
       <label htmlFor="feedback-textarea">
         Enter your feedback here, remeber to hashtag the company
       </label>
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charactersLeft}</p>
         <button>
           <span>Submit</span>
         </button>
